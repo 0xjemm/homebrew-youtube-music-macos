0988950d399a3017361923e8144ab0e9342c3dfc
jem <jeremyxarrabe@gmail.com>
2025-12-02 01:43:04 +0800

    fix: Update SHA256 hash for v1.0.0

--- Casks/youtube-music-macos.rb
+++ Casks/youtube-music-macos.rb
@@ -1,6 +1,6 @@
 cask "youtube-music-macos" do
   version "1.0.0"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "4ffa8d8f668d4e2bf683abf83b9483b722de6157e430b2fb6d090d01936876c2"
 
   url "https://github.com/0xjemm/youtube-music-macos/releases/download/v#{version}/YouTubeMusic.zip"
   name "YouTube Music"
